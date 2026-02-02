ad8f797abe8b12a128fccd438ff092b3c77bd852
AlliePinnell <x-rap@hotmail.ca>
2026-02-02 16:41:52 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -516,7 +516,7 @@ window.addEventListener('resize', changeRSDKImage);
 function versionNumber() {
     const ver = document.getElementById('versionNumber');
     if (ver){
-        ver.innerText = '0.1.20260201.0150a'
+        ver.innerText = '0.1.20260202.0441p'
     }
 }
 document.addEventListener("DOMContentLoaded", versionNumber);
